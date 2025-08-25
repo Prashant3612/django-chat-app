b8337fff3f9fcd0ab542ea6d2bd1cf6e92847690
Prashant3612 <102781765+Prashant3612@users.noreply.github.com>
2025-08-25 20:24:20 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -64,6 +64,7 @@ LOGIN_REDIRECT_URL = 'home/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
