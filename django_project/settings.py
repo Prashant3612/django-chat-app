01b3217d1e787e69539a9d29aadd7bcf39c00211
Prashant3612 <102781765+Prashant3612@users.noreply.github.com>
2025-08-25 20:40:38 +0530

    Update settings.py-->update database settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -103,10 +103,12 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
 }
 
 # Password validation
